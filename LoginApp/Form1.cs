d64f0604dce90fc1baa0c60bc26e6fa6b71c9b33
ashishLakhera <ashish.l@truevaluehub.com>
2023-08-02 00:17:40 +0530

    update code

--- LoginApp/Form1.cs
+++ LoginApp/Form1.cs
@@ -20,6 +20,7 @@ namespace LoginApp
         public Form1()
         {
             InitializeComponent();
+
         }
 
         private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
