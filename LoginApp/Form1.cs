3beb3fed4ec2f1db8147d8979bca5f26bf7f5b65
ashishLakhera <ashish.l@truevaluehub.com>
2023-08-01 17:40:52 +0530

    Add project files.

---
+++ LoginApp/Form1.cs
@@ -0,0 +1,151 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace LoginApp
+{
+    public partial class Form1 : Form
+    {
+        const string pwdKey = "wmdLoginPass";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Save_1(object sender, EventArgs e)
+        {
+            LoginDetail loginDetail = new LoginDetail();
+            List<LoginDetail> listLoginDetail = new List<LoginDetail>();
+            if (this.textBox1.Text != string.Empty && this.textBox2.Text != string.Empty)
+            {
+                loginDetail = new LoginDetail()
+                {
+                    UserName = this.textBox1.Text,
+                    Password = this.textBox2.Text,
+                    //WebSite = this.label4.Text,
+                    WebSite = "https://practicetestautomation.com/practice-test-login/",
+                };
+                loginDetail.Password = EncryptDecrypt.Encrypt(loginDetail.Password, pwdKey);
+            }
+            listLoginDetail.Add(loginDetail);
+
+            using (var writer = new StreamWriter("LoginInformation1.csv"))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csv.WriteRecords(listLoginDetail);
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Save_2(object sender, EventArgs e)
+        {
+            LoginDetail loginDetail = new LoginDetail();
+            List<LoginDetail> listLoginDetail = new List<LoginDetail>();
+
+            if (this.textBox3.Text != string.Empty && this.textBox4.Text != string.Empty)
+            {
+                loginDetail = new LoginDetail()
+                {
+                    UserName = this.textBox3.Text,
+                    Password = this.textBox4.Text,
+                    WebSite = this.label5.Text,
+                };
+                loginDetail.Password = EncryptDecrypt.Encrypt(loginDetail.Password, pwdKey);
+            }
+
+            listLoginDetail.Add(loginDetail);
+            using (var writer = new StreamWriter("LoginInformation2.csv"))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csv.WriteRecords(listLoginDetail);
+            }
+        }
+
+        private void login_1(object sender, EventArgs e)
+        {
+            string csvFile = "LoginInformation1.csv";
+            string userName, password, webSite;
+            ReadCSVFile(csvFile, out userName, out password, out webSite);
+            OpenWebUrl(webSite, userName, password);
+        }
+
+        private static void ReadCSVFile(string csvFile, out string userName, out string password, out string webSite)
+        {
+            var csvConfig = new CsvConfiguration(CultureInfo.CurrentCulture)
+            {
+                HasHeaderRecord = true,
+                Comment = '#',
+                AllowComments = true,
+                Delimiter = ",",
+            };
+            userName = string.Empty;
+            password = string.Empty;
+            webSite = string.Empty;
+            using (var streamReader = File.OpenText(csvFile))
+            using (var csvReader = new CsvReader(streamReader, csvConfig))
+            {
+                while (csvReader.Read())
+                {
+                    userName = csvReader.GetField(0);
+                    password = csvReader.GetField(1);
+                    webSite = csvReader.GetField(2);
+                }
+            }
+            password = EncryptDecrypt.Decrypt(password, pwdKey);
+        }
+
+        private void login_2(object sender, EventArgs e)
+        {
+
+        }
+        public void OpenWebUrl(string url, string userName, string password)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(url);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10000);
+
+            IWebElement inputAccount = driver.FindElement(By.Id("username"));
+            Thread.Sleep(2000);
+            inputAccount.Clear();
+            Thread.Sleep(2000);
+            inputAccount.SendKeys(userName);
+            Thread.Sleep(2000);
+
+            IWebElement inputPassword = driver.FindElement(By.Id("password"));
+
+            inputPassword.Clear();
+            Thread.Sleep(2000);
+            inputPassword.SendKeys(password);
+            Thread.Sleep(2000);
+
+            //IWebElement submitButton = driver.FindElement(By.XPath("/html/body/div[2]/form/table/tbody/tr[4]/td[2]/input"));
+            IWebElement submitButton = driver.FindElement(By.Id("submit"));
+            Thread.Sleep(2000);
+            submitButton.Click();
+            Thread.Sleep(2000);
+
+            //driver.Quit();
+        }
+    }
+}
---
+++ LoginApp/LoginPyFile.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+username = "student"
+password = "Password123"
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Chrome()
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Chrome()
+
+# head to github login page
+driver.get("https://practicetestautomation.com/practice-test-login/")
+# find username/email field and send the username itself to the input field
+driver.find_element("id", "username").send_keys(username)
+# find password input field and insert password as well
+driver.find_element("id", "password").send_keys(password)
+# click login button
+driver.find_element("id", "submit").click()
+# wait the ready state to be complete
+WebDriverWait(driver=driver, timeout=10).until(
+    lambda x: x.execute_script("return document.readyState === 'complete'")
+)
+error_message = "Incorrect username or password."
+# get the errors (if there are)
+errors = driver.find_elements("css selector", ".flash-error")
+# print the errors optionally
+# for e in errors:
+#     print(e.text)
+# if we find that error message within errors, then login is failed
+if any(error_message in e.text for e in errors):
+    print("[!] Login failed")
+else:
+    print("[+] Login successful")
